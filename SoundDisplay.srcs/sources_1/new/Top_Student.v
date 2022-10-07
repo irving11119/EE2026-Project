25f9f6bdf2e8e14187a2bed0fae00a044fa194f1
Irving <irving.deboer@gmail.com>
2022-10-07 16:48:08 +0800

    Added first two colours

--- SoundDisplay.srcs/sources_1/new/Top_Student.v
+++ SoundDisplay.srcs/sources_1/new/Top_Student.v
@@ -20,6 +20,7 @@ module Top_Student (
     input CLK100MHZ,
     input [15:0] sw,
     input btnC,
+    input bntU,
     input  J_MIC3_Pin3,   // Connect from this signal to Audio_Capture.v
     output J_MIC3_Pin1,   // Connect to this signal from Audio_Capture.v
     output J_MIC3_Pin4,  // Connect to this signal from Audio_Capture.v
@@ -36,7 +37,11 @@ module Top_Student (
     wire my_frame_begin, my_sendpix, my_sample_pixel;
     wire [12:0] my_pixel_index;
     wire my_chosen_clk;
+    wire [12:0]x;
+    wire [12:0]y;
     
+    reg [3:0] count = 3'd0;
+
     clk_divider clk_20k(.CLK(CLK100MHZ),.m(2499),.CLK_OUT(clk_20khz));
     clk_divider clk_10(.CLK(CLK100MHZ),.m(4999999),.CLK_OUT(clk_10hz));
     clk_divider clk_6p25(.CLK(CLK100MHZ),.m(7),.CLK_OUT(clk6p25m));
@@ -56,19 +61,34 @@ module Top_Student (
         );
 
     assign my_chosen_clk = sw[0] ? clk_10hz : clk_20khz;
-    
-    /*
-    always @ (posedge CLK100MHZ) begin
-        my_chosen_clk <= sw[0] ? clk_10hz : clk_20khz;
-    end
-    */
+    assign x = my_pixel_index % 96;
+    assign y = my_pixel_index / 96;
+
     always @ (posedge my_chosen_clk) begin
         led <= mic_in; //sum = 9+4 = 13 (R and B)
     end
 
     always @ (posedge CLK100MHZ) begin
-        oled_data <= {mic_in[11:7], 6'd0, mic_in[11:7]};
+        if (((x == 2 || x == 93) && (y >= 2 && y <= 60)) || ((y == 2 || y == 60) && (x > 2 && x < 93 )))
+        begin
+            oled_data <= {5'b11111, 6'd0, 5'd0};
+        end
+        
+        else if (((x >=4 && x<=6 || x>=89 && x<=91) && (y >=4 && y <=58)) || ((y >= 4 && y <=6 || y <= 58 && y >= 56)) && (x >= 4 && x <= 91))
+        begin
+            oled_data <= 16'hFFA500; 
+        end
+    
+        else 
+        begin
+            oled_data <= 16'd0;
+        end
     end
-    // Delete this comment and write your codes and instantiations here
+
+    always @(posedge CLK100MHZ, posedge bntU)
+    begin
+        if (bntU)
+            count <= 1;
+    end
 
 endmodule
